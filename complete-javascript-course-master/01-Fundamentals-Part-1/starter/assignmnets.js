b40e459541e68fd59e5281ba1ebf11071e22a1cd
Preetam Shri <preetamsshri@gmail.com>
2023-03-10 20:00:53 +0530

    Updated starter code

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignmnets.js
@@ -0,0 +1,21 @@
+// Values and variables in javascript
+let country = "India";
+let continent = "Asia";
+let population = 140;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+// data types in javascript
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+// let const and var difference
+language = 'Hindi';
+const currency = "INR";
+currency = 'USD';
+console.log(language);
+console.log(currency);
