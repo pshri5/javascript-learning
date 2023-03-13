24f7814a85b7f76d0d812318e07804b523ec3dc0
Preetam Shri <preetamsshri@gmail.com>
2023-03-13 11:32:45 +0530

    Functions and strict mode completed

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,5 @@
+'use strict';
+// Strict mode helps to not allow certain things and inform about certain eroors in the developer console. Without this the javascript progrm will just fail silently.
+
+//functions are piece of code that can be reused in a program
+
