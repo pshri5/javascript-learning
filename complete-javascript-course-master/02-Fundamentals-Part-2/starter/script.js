d7e5916ef881e2540872a09497f422b77db29d64
Preetam Shri <preetamsshri@gmail.com>
2023-03-14 12:10:29 +0530

    Arrays and its operations

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -11,4 +11,22 @@ describeCountry('Finland', 6, 'Helsinki');
 
 //coding challenge on functions
 
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
 
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log('Dolphins Win');
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log('Koalas Win');
+    }
+}
+
+// Arrays intro
+// Even though we use const to declare our array, we can change the data inside the array which seems like a contradiction to what we know about const but it is posssible because array is not a primitive value. Also we can change or mutate some values inside the array but we cannot replace the whole array.
+
+// Basic Array operations
